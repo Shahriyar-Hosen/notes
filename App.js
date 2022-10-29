5dead44e6dc8e661e896ac346abed0aca7f50db2
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-10-29 14:01:51 +0600

    user setup done & set storage

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import Intro from "./app/screens/Intro";
+import { useEffect } from "react";
 
 const App = () => {
+  const findUser = async () => {
+    const result = await AsyncStorage.getItem("user");
+    console.log(result);
+  };
+
+  useEffect(() => {
+    findUser();
+  }, []);
+
   return <Intro />;
 };
 
--- app/screens/Intro.js
+++ app/screens/Intro.js
@@ -1,24 +1,71 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
-import React from "react";
-import { StatusBar } from "expo-status-bar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  StatusBar,
+  Dimensions,
+} from "react-native";
+import RoundIconBtn from "../components/RoundIconBtn";
+import colors from "../misc/colors";
 
-const Intro = () => {
+const Intro = ({ onFinish }) => {
+  const [name, setName] = useState("");
+
+  const handleOnChangeText = (text) => setName(text);
+
+  const handleSubmit = async () => {
+    const user = { name: name };
+    await AsyncStorage.setItem("user", JSON.stringify(user));
+    if (onFinish) onFinish();
+  };
+
   return (
     <>
       <StatusBar hidden />
       <View style={styles.container}>
-        <Text>Enter Your Name to Continue</Text>
-        <TextInput />
+        <Text style={styles.inputTitle}>Enter Your Name to Continue</Text>
+        <TextInput
+          value={name}
+          onChangeText={handleOnChangeText}
+          placeholder="Enter Name"
+          style={styles.textInput}
+        />
+
+        {/* If Text value > 3 than show Button */}
+        {name.trim().length >= 3 ? (
+          <RoundIconBtn antIconName="arrowright" onPress={handleSubmit} />
+        ) : null}
       </View>
     </>
   );
 };
 
+const width = Dimensions.get("window").width - 50;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: colors.PRIMARY,
+    color: colors.PRIMARY,
+    width,
+    height: 50,
+    borderRadius: 10,
+    paddingLeft: 15,
+    fontSize: 25,
+    marginBottom: 15,
+  },
+  inputTitle: {
+    alignSelf: "flex-start",
+    paddingLeft: 25,
+    marginBottom: 5,
+    opacity: 0.5,
   },
 });
 
