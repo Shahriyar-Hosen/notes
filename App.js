93f71c1513de222082fd7fa2c5bc8bd4192b2749
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-10-29 16:58:57 +0600

    user login done

--- App.js
+++ App.js
@@ -2,19 +2,28 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Intro from "./app/screens/Intro";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import NoteScreen from "./app/screens/NoteScreen";
 
 const App = () => {
+  const [user, setUser] = useState({});
+
   const findUser = async () => {
     const result = await AsyncStorage.getItem("user");
+    if (result !== null) {
+      setUser(JSON.parse(result));
+    }
     console.log(result);
   };
 
   useEffect(() => {
     findUser();
+    // AsyncStorage.clear()
   }, []);
 
-  return <Intro />;
+  if (!user?.name) return <Intro onFinish={findUser} />;
+
+  return <NoteScreen user={user} />;
 };
 
 export default App;
---
+++ app/components/NoteInputModal.js
@@ -0,0 +1,12 @@
+import { View, Text, Modal } from "react-native";
+import React from "react";
+
+const NoteInputModal = () => {
+  return (
+    <Modal>
+      <Text>NoteInputModal</Text>
+    </Modal>
+  );
+};
+
+export default NoteInputModal;
